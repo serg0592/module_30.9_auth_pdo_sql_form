2ee47c1f0ed8f1df25e3c84e38827c40b02e141b
Sergey Kudelin <kudelin.work@bk.ru>
2022-06-17 11:43:00 +0300

    Update(1)

--- core/views/login_view.php
+++ core/views/login_view.php
@@ -1,5 +1,9 @@
+<h1>Авторизация</h1>
 <form method="POST">
     Логин <input name="login" type="text" required><br>
     Пароль <input name="password" type="password" required><br>
     <input name="submit" type="submit" value="Войти">
+</form>
+<form method="POST" action="../views/registration_view.php">
+    <input name="submit" type="submit" value="Регистрация">
 </form>
