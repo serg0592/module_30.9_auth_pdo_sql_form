f3e89b19040b68576239ef9c075db4d6ce6091a3
Sergey Kudelin <kudelin.work@bk.ru>
2022-06-23 19:49:07 +0300

    Update: download interface added

--- Application/views/download_pic_view.php
+++ Application/views/download_pic_view.php
@@ -1,1 +1,8 @@
 <h1>Интерфейс для загрузки изображений</h1>
+<form method='post' action="/index.php" enctype="multipart/form-data">          
+    <input type="hidden" name="MAX_FILE_SIZE" value="5000000">
+    <input type='file' name='file[]' class='file-drop' id='file-drop' multiple required><br> 
+    <input type='submit' value='Загрузить' >
+</form>
+ 
+<div class='message-div message-div_hidden' id='message-div'></div>
